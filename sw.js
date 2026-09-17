2db917c471b45172f140e56436d86523e0576c9d
JairoSilvar <jairo@bahh.in>
2026-09-17 20:48:48 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="total-hits-shell-v3";
+const CACHE="total-hits-shell-v8";
 const SHELL=["./","./manifest.json","./total-hits-logo.svg","./total-hits-wordmark.svg","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
